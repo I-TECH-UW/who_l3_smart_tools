1a07b0b72baec8569ffc49aa0052c04281dc006b
Ian <ian.c.bacher@gmail.com>
2024-06-27 16:48:51 -0400

    Add Questionnaire generator

---
+++ tests/test_questionnaire_generator.py
@@ -0,0 +1,14 @@
+import os
+import unittest
+from who_l3_smart_tools.core.questionnaires.questionnaire_generator import QuestionnaireGenerator
+
+
+class TestQuestionnaireGenerator(unittest.TestCase):
+    def setUp(self):
+        self.input_file = os.path.join("tests", "data", "l2", "test_dd.xlsx")
+        self.output_dir = os.path.join("tests", "output", "fsh", "questionnaires")
+
+    def test_full_data_dictionary(self):
+        generator = QuestionnaireGenerator(self.input_file, self.output_dir)
+
+        generator.generate_fsh_from_excel()
