1ea7fd68554a5e8b99f6695a336bdfcceb731c53
xkmato <kenneth@uvotam.com>
2024-07-02 11:21:31 +0300

    add some documentation for the teminology command

--- who_l3_smart_tools/cli/terminology.py
+++ who_l3_smart_tools/cli/terminology.py
@@ -8,6 +8,23 @@ from who_l3_smart_tools.core.terminology.who.terminology import HIVTerminology
 
 
 def main():
+    """
+    Generate HIV Concept Terminology.
+
+    This function parses command line arguments to generate HIV Concept Terminology.
+    It requires either an Excel file or a list of CSV files containing the HIV terminology data.
+    The generated terminology can be outputted in either CSV or JSON format.
+
+    Command line arguments:
+    -o, --output-dir: Output directory for the generated HIV Concept Terminology.
+    -e, --excel-file: Excel file containing the HIV terminology data.
+    -f, --files: List of CSV files containing the HIV terminology data.
+    -of, --output-format: Output format for the generated HIV Concept Terminology. (default: csv)
+
+    Raises:
+    - ValueError: If neither an Excel file nor CSV files are provided.
+
+    """
     argparser = argparse.ArgumentParser(description="Generate HIV Concept Terminology.")
 
     files_group = argparser.add_mutually_exclusive_group(required=True)
@@ -45,7 +62,8 @@ def main():
     elif args.files:
         hiv_terminology = HIVTerminology(args.files)
     else:
-        raise ValueError("Either and excel file or csv files must be provided.")
+        raise ValueError("Either an Excel file or CSV files must be provided.")
+
     if args.output_format == "json":
         hiv_terminology.process_concept_for_json(os.path.join(args.output_dir, "hiv_concepts.json"))
     else:
