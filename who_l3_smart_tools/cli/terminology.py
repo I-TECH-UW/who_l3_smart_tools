720522753f1e284f34955c10b179f8d9662e0349
xkmato <kenneth@uvotam.com>
2024-07-04 17:50:27 +0300

    only excel support

--- who_l3_smart_tools/cli/terminology.py
+++ who_l3_smart_tools/cli/terminology.py
@@ -20,8 +20,7 @@ def main():
     Command line arguments:
     -o, --output-dir: Output directory for the generated HIV Concept Terminology.
     -e, --excel-file: Excel file containing the HIV terminology data.
-    -f, --files: List of CSV files containing the HIV terminology data.
-    -of, --output-format: Output format for the generated HIV Concept Terminology. (default: csv)
+    -f, --output-format: Output format for the generated HIV Concept Terminology. (default: csv)
     -v, --values-set: File where to write valuesets.
 
     Raises:
@@ -30,28 +29,19 @@ def main():
     """
     argparser = argparse.ArgumentParser(description="Generate HIV Concept Terminology.")
 
-    files_group = argparser.add_mutually_exclusive_group(required=True)
-
     argparser.add_argument(
         "-o",
         "--output-dir",
         required=True,
         help="Output directory for the generated HIV Concept Terminology.",
     )
-    files_group.add_argument(
+    argparser.add_argument(
         "-e",
         "--excel-file",
         help="Excel file containing the HIV terminology data.",
     )
-    files_group.add_argument(
-        "-f",
-        "--files",
-        nargs="+",
-        help="List of CSV files containing the HIV terminology data. With concept class separated by a colon.\
-        Example: '/home/ubuntu/files/file1.csv:concept_class1 /home/ubuntu/files/file2.csv:concept_class2'",
-    )
     argparser.add_argument(
-        "-of",
+        "-f",
         "--output-format",
         default="csv",
         choices=["csv", "json"],
@@ -66,13 +56,8 @@ def main():
 
     os.makedirs(args.output_dir, exist_ok=True)
 
-    if args.excel_file:
-        hiv_terminology = HIVTerminology.from_excel(args.excel_file)
-    elif args.files:
-        hiv_terminology = HIVTerminology(args.files)
-    else:
-        raise ValueError("Either an Excel file or CSV files must be provided.")
 
+    hiv_terminology = HIVTerminology.from_excel(args.excel_file)
     if args.output_format == "json":
         hiv_terminology.process_concept_for_json(os.path.join(args.output_dir, "hiv_concepts.json"))
     else:
