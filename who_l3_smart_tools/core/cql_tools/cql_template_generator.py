beed4809051f44e1801a44e9acec3c512fccce1f
Piotr Mankowski <pmanko@uw.edu>
2024-07-11 13:41:40 -0700

    Jinja2

--- who_l3_smart_tools/core/cql_tools/cql_template_generator.py
+++ who_l3_smart_tools/core/cql_tools/cql_template_generator.py
@@ -1,7 +1,7 @@
-import re
 from typing import Any
 import stringcase
 import pandas as pd
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from who_l3_smart_tools.utils.cql_helpers import (
     determine_scoring_suggestion,
@@ -9,69 +9,140 @@ from who_l3_smart_tools.utils.cql_helpers import (
     create_cql_concept_dictionaries,
 )
 
+# Initialize Jinja2 environment
+env = Environment(
+    loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
+)
+
+# AutoGen Markers
+start_marker = "/* AUTO-GENERATED START */"
+end_marker = "/* AUTO-GENERATED END */"
 
 # Templates
-cql_file_header_template = """/**
- * Library: {dak_id} Logic
- * Ref No: {ref_no_}
- * Short Name: {short_name}
- *
- * Definition: {indicator_definition}
- *
- * Numerator: {numerator_definition}
- * Numerator Calculation: {numerator_calculation}
- * Numerator Exclusions: {numerator_exclusions}
- *
- * Denominator: {denominator_definition}
- * Denominator Calculation: {denominator_calculation}
- * Denominator Exclusions: {denominator_exclusions}
- *
- * Disaggregations:
- """
-cql_file_header_disaggregation_list_template = """* {disaggregation_description}
- """
-cql_file_header_second_template = """* 
- * Disaggregation Elements: {disaggregation_data_elements}
- *
- * Numerator and Denominator Elements:
- * {list_of_all_data_elements_included_in_numerator_and_denominator}
- *
- * Reference: {reference}
- * 
- * Data Concepts:
- * """
-
-cql_file_header_data_concept_template = """ 
- * {id}: {label} | {description}"""
-
-
-cql_file_header_additional_context_template = """
- *
- * Additional Context
- * - what it measures: {what_it_measures}
- * - rationale: {rationale}
- * - method: {method_of_measurement}
- * 
- * Suggested Scoring Method: {scoring_method} | {scoring_instance}
+master_template = env.from_string(
+    """
+{{start_marker}}
+/**
+ * CQL Indicator File
  */
 
-library {cql_library_name}
+{{ header }}
+                                  
+{% if proportion %}
+  {{ proportion }}
+{% elif continuous_variable %}
+  {{ continuous_variable }}
+{% endif %}
+{{end_marker}}
+
+{% if dissagrations %}
+    {{ disaggregations }}
+{% endif %}
+"""
+)
+
+header_template = env.from_string(
+    """
+/**
+ * Library: {{ dak_id }} Logic
+ * Ref No: {{ ref_no_ }}
+ * Short Name: {{ short_name }}
+ *
+ * Definition: {{ indicator_definition }}
+ *
+ * Numerator: {{ numerator_definition }}
+ * Numerator Calculation: {{ numerator_calculation }}
+ * Numerator Exclusions: {{ numerator_exclusions }}
+ *
+ * Denominator: {{ denominator_definition }}
+ * Denominator Calculation: {{ denominator_calculation }}
+ * Denominator Exclusions: {{ denominator_exclusions }}
+ *
+ * Disaggregations:
+{% for disaggregation in disaggregations %}
+ * {{ disaggregation }}
+{% endfor %}
+ *
+ * Disaggregation Elements: {{ disaggregation_data_elements }}
+ *
+ * Numerator and Denominator Elements:
+{% for element in all_data_elements %}
+ * {{ element }}
+{% endfor %}
+ *
+ * Reference: {{ reference }}
+ * 
+ * Data Concepts:
+{% for concept in data_concepts %}
+ * {{ concept.id }}: {{ concept.label }} | {{ concept.description }}
+{% endfor %}
+ *
+ * Additional Context
+ * - what it measures: {{ what_it_measures }}
+ * - rationale: {{ rationale }}
+ * - method: {{ method_of_measurement }}
+ * 
+ * Suggested Scoring Method: {{ scoring_method }} | {{ scoring_instance }}
+ */
+
+library {{ cql_library_name }}
 
 // Included Libraries
 using FHIR version '4.0.1'
 
-include HIVIndicatorCommon version '0.0.1' called HIC
+include HIVCommon version '0.0.1' called HIC
 include FHIRHelpers version '4.0.1'
-include WHOCommon called WCom
 include FHIRCommon called FC
+include WHOCommon called WCom
 
 // Indicator Definition
-parameter "Measurement Period" Interval<Date> default Interval[@2023-01-01, @2020-01-30]
+parameter "Measurement Period" Interval<Date> default Interval[@2023-01-01, @2023-01-30]
 
 context Patient
 """
+)
 
-cql_file_initial_population_template = """
+proportion_template = env.from_string(
+    """
+/**
+ * Proportion Template
+ */
+
+{{ initial_population }}
+
+{{ numerator }}
+
+{{ denominator }}
+"""
+)
+
+continuous_variable_template = env.from_string(
+    """
+/**
+ * Continuous Variable Template
+ */
+
+{{ initial_population }}
+
+{{ measure_population }}
+
+{{ measure_observation }}
+"""
+)
+
+measure_observation_template = env.from_string(
+    """                                         
+/**
+ * Measure Observation Template
+ */
+
+define "Measure Observation":
+  true                                     
+"""
+)
+
+cql_file_initial_population_template = env.from_string(
+    """
 /*
  *Initial Population
  */
@@ -79,37 +150,97 @@ cql_file_initial_population_template = """
 define "Initial Population":
   true
 """
+)
 
-cql_file_numerator_template = """
+cql_file_numerator_template = env.from_string(
+    """
 /**
- * Numerator: {numerator_definition}
- * Numerator Calculation: {numerator_calculation}
+ * Numerator
+ * 
+ * Definition: {{ numerator_definition }}
+ * Calculation: {{ numerator_calculation }}
  */
 
 define "Numerator":
-  true 
+  true
 """
+)
 
-cql_file_denominator_template = """
+cql_file_denominator_template = env.from_string(
+    """                                          
 /**
- * Denominator: {denominator_definition}
- * Denominator Calculation: {denominator_calculation}
+ * Denominator
+ *
+ * Definition: {{ denominator_definition }}
+ * Calculation: {{ denominator_calculation }}
  */
 
 define "Denominator":
   true
 """
+)
 
-cql_file_measure_population_template = """
+cql_file_measure_population_template = env.from_string(
+    """                                           
 /**
  * Measure Population
+ *
+ * Definition: {{ measure_population_definition }}
+ * Calculation: {{ measure_population_calculation }}
  */
-
+                                                       
 define "Measure Population":
   true
 """
+)
+
+cql_file_measure_observation_template = env.from_string(
+    """
+/**
+ * Measure Observation
+ * Definition:
+ * Calculation:                                                       
+ */     
+
+define function "Measure Observation"(Patient "Patient"):
+  1
+"""
+)
+
+cql_file_disaggregations_template = env.from_string(
+    """
+/**
+ * Disaggregators
+ * 
+ */
+{% for disaggregation in disaggregations %}
+define "{{ disaggregation.name }}":
+  HIC."{{ disaggregation.name }} Stratifier"
+"""
+)
+
 
 class CqlTemplateGenerator:
+    """
+    This class represents a CQL template generator that is used to generate CQL scaffolds for each indicator in the DAK.
+
+    Attributes:
+        indicator_artifact_file (str): The path to the indicator artifact file.
+        data_dictionary_file (str): The path to the data dictionary file.
+        cql_scaffolds (dict[str, str]): A dictionary containing the generated CQL scaffolds, with indicator names as keys and scaffolds as values.
+        concept_lookup (dict[str, Any]): A dictionary containing the concept lookup data.
+        cql_concept_dictionary (dict[str, Any]): A dictionary containing the CQL concept dictionary data.
+        data_dictionary_xls (pd.ExcelFile): The data dictionary Excel file.
+        indicator_artifact (pd.DataFrame): The indicator artifact data frame.
+        dak_name (str): The DAK name.
+
+    There is limited regeneration of existing CQL files. The header section up to the // AUTO-GENERATED END comment is updated.
+
+    The other sections are updated only if the input file is empty after the AUTO-GENERATED END comment. Otherwise, a new file is created with a
+    -AG suffix.
+
+    """
+
     def __init__(self, indicator_artifact_file: str, data_dictionary_file: str):
         self.indicator_artifact_file = indicator_artifact_file
         self.data_dictionary_file = data_dictionary_file
@@ -129,14 +260,22 @@ class CqlTemplateGenerator:
 
         self.dak_name = get_dak_name(self.data_dictionary_xls)
 
-        self.concept_lookup, self.cql_concept_dictionary = create_cql_concept_dictionaries()
-
+        self.concept_lookup, self.cql_concept_dictionary = (
+            create_cql_concept_dictionaries()
+        )
 
     def print_to_files(self, output_dir: str, update_existing: bool = True):
         """
         This method writes the CQL scaffolds to files in the output directory.
+
+        Args:
+            output_dir (str): The directory where the CQL files will be written.
+            update_existing (bool, optional): Whether to update existing files or create new ones. Defaults to True.
         """
-        last_generated_line = ["include FHIRCommon called FC\n", "using FHIR version '4.0.1'\n"]
+        last_generated_line = [
+            "include FHIRCommon called FC\n",
+            "using FHIR version '4.0.1'\n",
+        ]
 
         for indicator_name, scaffold in self.cql_scaffolds.items():
             file_name = indicator_name.replace(".", "")
@@ -147,8 +286,12 @@ class CqlTemplateGenerator:
                 with open(f"{output_dir}/{file_name}Logic.cql", "r") as file:
                     # Read up to the last generated line
                     lines = file.readlines()
-                    last_generated_line_index = lines.index(last_generated_line[0]) if last_generated_line[0] in lines else lines.index(last_generated_line[1])
-                    if last_generated_line_index == -1:     
+                    last_generated_line_index = (
+                        lines.index(last_generated_line[0])
+                        if last_generated_line[0] in lines
+                        else lines.index(last_generated_line[1])
+                    )
+                    if last_generated_line_index == -1:
                         raise ValueError(
                             f"Could not find last generated line in {file_name}Logic.cql"
                         )
@@ -162,6 +305,9 @@ class CqlTemplateGenerator:
     def generate_cql_scaffolds(self):
         """
         This method generates CQL scaffolds for each indicator in the DAK.
+
+        Returns:
+            dict[str, str]: A dictionary containing the generated CQL scaffolds, with indicator names as keys and scaffolds as values.
         """
 
         # Generate CQL scaffolds for each indicator
@@ -169,7 +315,7 @@ class CqlTemplateGenerator:
 
         for index, row in self.indicator_artifact.iterrows():
             if row["Included in DAK"]:
-                indicator_name, scaffold = self.generate_cql_header(row)
+                indicator_name, scaffold = self.generate_cql_indicator_file(row)
                 cql_scaffolds[indicator_name] = scaffold
 
         self.cql_scaffolds = cql_scaffolds
@@ -180,96 +326,17 @@ class CqlTemplateGenerator:
         """
         This method provides scaffolding for CQL templates that includes
         indicator details from the DAK to facilitate CQL authoring.
+
+        Args:
+            row_content (pd.Series): The row content containing indicator details.
+
+        Returns:
+            tuple[str, str]: A tuple containing the DAK ID and the filled CQL header template.
         """
+        # ... (rest of the code)
 
-        # Columns: DAK ID	Ref no.	Short name	Indicator definition	Numerator calculation	Numerator exclusions	Denominator calculation	Denominator exclusions	Disaggregation data elements	List of all data elements included in numerator and denominator	Numerator definition	Denominator definition	Disaggregation description	Comments and references	Reference	Page no.	GAM2023 alignment	GF2022 alignment	MER2.6.1 alignment	WHO2020 alignment	Survey based	Included in DAK	Priority	Core	New	Updated	Category	What it measures	Rationale	Method of measurement
-        # Column list:
-        # - DAK ID
-        # - Ref no.
-        # - Short name
-        # - Indicator definition
-        # - Numerator calculation
-        # - Numerator exclusions
-        # - Denominator calculation
-        # - Denominator exclusions
-        # - Disaggregation data elements
-        # - List of all data elements included in numerator and denominator
-        # - Numerator definition
-        # - Denominator definition
-        # - Disaggregation description
-        # - Comments and references
-        # - Reference
-        # - Page no.
-        # - GAM2023 alignment
-        # - GF2022 alignment
-        # - MER2.6.1 alignment
-        # - WHO2020 alignment
-        # - Survey based
-        # - Included in DAK
-        # - Priority
-        # - Core
-        # - New
-        # - Updated
-        # - Category
-        # - What it measures
-        # - Rationale
-        # - Method of measurement
 
-        # Convert row_content to a dictionary and process it to remove newlines
-        raw_row_dict = row_content.to_dict()
-        row_dict = {}
-
-        dak_id: str = raw_row_dict["DAK ID"]
-        cql_library_name = f"{dak_id.replace(".", "")}Logic"
-        ref_no = raw_row_dict["Ref no."]
-        denominator_val = raw_row_dict["Denominator calculation"]
-
-        for key, value in raw_row_dict.items():
-            if isinstance(value, list):
-                value = " ".join(str(v) for v in value)
-            if isinstance(value, str):
-                value = value.replace("\n", " | ")
-            if pd.isna(value):
-                value = ""
-
-            key = stringcase.snakecase(stringcase.lowercase(key))
-            row_dict[key] = value
-
-        # Add library name
-        row_dict["cql_library_name"] = cql_library_name
-
-        # get scoring variables using the determine_scoring method
-        (
-            row_dict["scoring_method"],
-            row_dict["scoring_title"],
-            row_dict["scoring_instance"],
-        ) = determine_scoring_suggestion(denominator_val)
-
-        # Dissagregation parsing
-        disaggregation_data_elements = row_dict["disaggregation_description"].split("|")
-
-        # Generate header using templates
-        filled_template: str = cql_file_header_template.format(**row_dict)
-
-        for disaggregation in disaggregation_data_elements:
-            disaggregation = disaggregation.strip()
-            filled_template += cql_file_header_disaggregation_list_template.format(
-                disaggregation_description=disaggregation
-            )
-
-        filled_template += cql_file_header_second_template.format(**row_dict)
-
-        if ref_no in self.concept_lookup.keys():
-            for concept in self.concept_lookup[ref_no]:
-                if pd.isna(concept["id"]):
-                    continue
-                filled_template += cql_file_header_data_concept_template.format(
-                    **concept
-                )
-
-        filled_template += cql_file_header_additional_context_template.format(
-            **row_dict
-        )
-
-        # Using the format method to fill in the template with row content
-        return dak_id, filled_template
+# Example usage
+generator = CqlTemplateGenerator("indicator_artifact.xlsx", "data_dictionary.xlsx")
+generator.generate_cql_scaffolds()
+generator.print_to_files("output_dir")
