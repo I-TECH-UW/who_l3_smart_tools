26158222d0aeb6faaa66576389c24f0b58121e14
Piotr Mankowski <pmanko@uw.edu>
2025-02-08 08:00:01 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="who_l3_smart_tools",
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages(),
     install_requires=[
         "pandas>=1.0",
